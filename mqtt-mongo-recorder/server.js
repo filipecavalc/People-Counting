9ed8bfb2b9066c1f0e1b5e65b2369b5f3d72022d
Filipe <filipecavalc@gmail.com>
2018-06-15 15:49:52 -0300

    documentação da captura de dados
    Código permite ouvir o broker mqtt e salvar os dados no banco de dados MongoDB

---
+++ mqtt-mongo-recorder/server.js
@@ -0,0 +1,49 @@
+/**
+ *
+ * This NodeJS application listens to MQTT messages and records them to MongoDB
+ *
+ * @author  Dennis de Greef <github@link0.net>
+ * @license MIT
+ *
+ */
+var mongodb  = require('mongodb');
+var mqtt     = require('mqtt');
+var config   = require('./config');
+
+var mqttUri  = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
+var client   = mqtt.connect(mqttUri);
+
+client.on('connect', function () {
+    client.subscribe(config.mqtt.namespace);
+});
+
+var mongoUri = 'mongodb://' + config.mongodb.hostname + ':' + config.mongodb.port + '/' + config.mongodb.database;
+mongodb.MongoClient.connect(mongoUri, function(error, database) {
+    if(error != null) {
+        throw error;
+    }
+
+    var collection = database.collection(config.mongodb.collection);
+    collection.createIndex( { "topic" : 1 } );
+
+    client.on('message', function (topic, message) {
+      try {
+        console.log(message.toString());
+          var messageObject = {
+              topic: topic,
+              message: JSON.parse(message.toString())
+          };
+
+          collection.insert(messageObject, function(error, result) {
+              if(error != null) {
+                  console.log("ERROR: " + error);
+              }
+          });
+      } catch (e) {
+        console.log("Mensagem fora do padrão JSON");
+      } finally {
+
+      }
+
+    });
+});
